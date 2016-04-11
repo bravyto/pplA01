6ce98bfc9deb1e0cabdab576ba07b7d5a6bcab4f
tryvianariska <tryvianariska@gmail.com>
2016-04-11 09:39:30 +0700

    perubahan main dan menu activity
    sudah terintegrasi

--- app/src/main/java/ppla01/foodo/JadwalMakanActivity.java
+++ app/src/main/java/ppla01/foodo/JadwalMakanActivity.java
@@ -2,8 +2,8 @@ package ppla01.foodo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,14 +17,19 @@ import java.util.Date;
 /**
  * Created by Bravyto on 10/04/2016.
  */
-public class JadwalMakanActivity extends AppCompatActivity {
+public class JadwalMakanActivity extends Activity{
+    SharedPreferences spref;
+    SharedPreferences.Editor editor;
 
-    protected Button set_eat_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwal_makan);
+
+        spref = getApplicationContext().getSharedPreferences("my_data", 0);
+        editor = spref.edit();
 
         SimpleDateFormat sdf = new SimpleDateFormat("hh:ss");
         Date date = null;
@@ -58,15 +63,12 @@ public class JadwalMakanActivity extends AppCompatActivity {
         TimePicker picker3 = (TimePicker) findViewById(R.id.dinner);
         picker3.setCurrentHour(c.get(Calendar.HOUR_OF_DAY));
         picker3.setCurrentMinute(c.get(Calendar.MINUTE));
-
-        set_eat_time = (Button) findViewById(R.id.submitEatTime);
-        set_eat_time.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent i = new Intent(JadwalMakanActivity.this, MenuActivity.class);
-                startActivity(i);
-            }
-        });
+    }
+    public void save(View view){
+        editor = spref.edit();
+        editor.putString("log", "1");
+        editor.commit();
+        Intent intent = new Intent(JadwalMakanActivity.this, MenuActivity.class);
+        startActivity(intent);
     }
 }
