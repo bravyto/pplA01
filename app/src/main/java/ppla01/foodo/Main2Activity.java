2294955ce544e09fadc0a3284fc390c49897f04b
bravyto <bravytotp@gmail.com>
2016-05-15 07:14:45 +0700

    commit vyto
    implementasi evaluation page

--- app/src/main/java/ppla01/foodo/Main2Activity.java
+++ app/src/main/java/ppla01/foodo/Main2Activity.java
@@ -85,10 +85,6 @@ public class Main2Activity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         setTitle("Home");
-
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        getSupportActionBar().setIcon(R.mipmap.ic_actionbaricon);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
---
+++ app/src/main/java/ppla01/foodo/WeekEvaluationActivity.java
@@ -0,0 +1,40 @@
+package ppla01.foodo;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Bravyto on 14/05/2016.
+ */
+public class WeekEvaluationActivity extends AppCompatActivity {
+
+    SharedPreferences spref;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#F44336")));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setTitle("Week Evaluation");
+
+        spref = getApplicationContext().getSharedPreferences("my_data", 0);
+        editor = spref.edit();
+
+
+    }
+}
