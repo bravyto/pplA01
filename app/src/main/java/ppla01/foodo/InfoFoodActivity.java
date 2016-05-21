edd2c965fb7a24577fce520f728b62d7583b19bc
tryvianariska <tryvianariska@gmail.com>
2016-05-21 08:03:06 +0700

    cek

--- app/src/main/java/ppla01/foodo/InfoFoodActivity.java
+++ app/src/main/java/ppla01/foodo/InfoFoodActivity.java
@@ -56,12 +56,12 @@ public class InfoFoodActivity extends AppCompatActivity {
         extras = intent.getExtras();
 
         foodName = extras.getString(FoodActivity.EXTRA_MESSAGE1);
-        calories = "Food Calories: " + extras.getString(FoodActivity.EXTRA_MESSAGE2);
-        String vitc = "Vit C: " + extras.getString(FoodActivity.EXTRA_MESSAGE3);
-        String protein = "Protein: " + extras.getString(FoodActivity.EXTRA_MESSAGE4);
-        String carbo = "Carbohydrat: " + extras.getString(FoodActivity.EXTRA_MESSAGE5);
-        String water = "Water: " + extras.getString(FoodActivity.EXTRA_MESSAGE6);
-        String calcium = "Calcium: " + extras.getString(FoodActivity.EXTRA_MESSAGE7);
+        calories        = "Food Calories: " + extras.getString(FoodActivity.EXTRA_MESSAGE2);
+        String vitc     = "Vit C: " + extras.getString(FoodActivity.EXTRA_MESSAGE3);
+        String protein  = "Protein: " + extras.getString(FoodActivity.EXTRA_MESSAGE4);
+        String carbo    = "Carbohydrat: " + extras.getString(FoodActivity.EXTRA_MESSAGE5);
+        String water    = "Water: " + extras.getString(FoodActivity.EXTRA_MESSAGE6);
+        String calcium  = "Calcium: " + extras.getString(FoodActivity.EXTRA_MESSAGE7);
         String porsiAndBerat = "Portion: " + extras.getString(FoodActivity.EXTRA_MESSAGE8) + " Weight: " + extras.getString(FoodActivity.EXTRA_MESSAGE9) + "(g)";
 
         kalori = extras.getString(FoodActivity.EXTRA_MESSAGE2);;
