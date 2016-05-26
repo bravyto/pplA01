9edad238c5a06b4570a6024662922a1bac8b9ff0
gemaraditya <gema.raditya@gmail.com>
2016-05-26 13:26:39 +0700

    set vibrate and lights
    set

--- app/src/main/java/ppla01/foodo/NotifyService.java
+++ app/src/main/java/ppla01/foodo/NotifyService.java
@@ -11,6 +11,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -119,6 +120,9 @@ public class NotifyService extends Service{
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
         mBuilder.setContentIntent(resultPendingIntent);
+
+        mBuilder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+        mBuilder.setLights(Color.YELLOW, 3000, 3000);
 
         mBuilder.setAutoCancel(true);
 
--- app/src/main/java/ppla01/foodo/NotifyServiceNoon.java
+++ app/src/main/java/ppla01/foodo/NotifyServiceNoon.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Binder;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
@@ -111,6 +112,9 @@ public class NotifyServiceNoon extends Service {
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
         mBuilderNoon.setContentIntent(resultPendingIntent);
+
+        mBuilderNoon.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+        mBuilderNoon.setLights(Color.YELLOW, 3000, 3000);
 
         mBuilderNoon.setAutoCancel(true);
 
